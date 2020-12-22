7e6bfdfb0c89c71fcfe98dc2ccaa31bab8cd01e5
Rodrigo <rodrigorael53@gmail.com>
2020-12-22 02:05:55 -0300

    Final project

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { motion } from 'framer-motion';
+
 import { Container, Text, Input, WhatsappButton } from './styles';
 
 import LinkList from '../../components/LinkList';
@@ -11,18 +13,30 @@ export default function Form() {
     <Container>
       <Text>Enter password</Text>
       
-      <Input 
+      <Input
         value={password} 
         onChange={
           event => setPassword(event.target.value)
         } 
       />
+
         
       {
         password !== 'Rivo22/12'
           ? <LinkList />
           : <WhatsappButton href="https://chat.whatsapp.com/FHbXxrGHbuoFIdn69hBxXO" >
-              Proceed
+              <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{ rotate: 360, scale: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 150
+                  }}
+                >
+                  
+                  Proceed
+                </motion.div>
             </WhatsappButton>
       }
       
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { motion } from 'framer-motion';
+
 import { Container, Header, Title } from './styles';
 
 import Form from '../../components/Form';
@@ -7,11 +9,21 @@ import Form from '../../components/Form';
 export default function HomePage() {
   return (
     <Container>
-      <Header>
-        <Title>Hello Rivo</Title>
-        <Title>welcome to the game</Title>
-      </Header>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ rotate: 360, scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 60
+        }}
+      >
+        <Header>
+          <Title>Hello Rivo</Title>
+          <Title>welcome to the game</Title>
+        </Header>
 
+      </motion.div>
       <Form />
     </Container>
   );
